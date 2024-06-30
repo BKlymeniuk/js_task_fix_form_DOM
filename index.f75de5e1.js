3b973d7aad4665626dcecc5705628efb10d15b0a
Bohdan Klymeniuk <klymeniuk.bohdan@gmail.com>
2024-06-30 13:15:17 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,21 @@
+"use strict";
+const inputs = [
+    ...document.querySelectorAll("input")
+];
+inputs.forEach((input)=>{
+    const label = document.createElement("label");
+    const inputName = capitalizeFirstLetter(splitWords(input.name));
+    label.className = "field-label";
+    label.textContent = inputName;
+    label.htmlFor = input.id;
+    input.parentElement.insertBefore(label, input);
+    input.placeholder = inputName;
+});
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+function splitWords(string) {
+    return string.replace(/([A-Z])/g, " $1");
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
